60738b7ff46cc8f36b089dd5d70119ab67102474
selvaAU <selva.mariappan@anywhere.co>
2023-03-13 00:41:17 +0530

    Added few configurations for war file

--- src/main/java/com/example/mybooksapp/MybooksappApplication.java
+++ src/main/java/com/example/mybooksapp/MybooksappApplication.java
@@ -2,12 +2,18 @@ package com.example.mybooksapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MybooksappApplication {
+public class MybooksappApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybooksappApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MybooksappApplication.class, args);
+  }
+
+//  @Override
+//  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//    return builder.sources(MybooksappApplication.class);
+//  }
 
 }
